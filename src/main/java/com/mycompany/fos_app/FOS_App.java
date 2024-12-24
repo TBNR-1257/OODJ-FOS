b39fbe6cb72801cd71d74a3a71205111111808ea
TBNR-1257 <tzehernbryan22w@forwardschool.edu.my>
2024-12-24 19:01:15 +0800

    First Commit

---
+++ src/main/java/com/mycompany/fos_app/FOS_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fos_app;
+
+/**
+ *
+ * @author User
+ */
+public class FOS_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
